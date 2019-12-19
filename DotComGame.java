7859e216548595194d3af65a28e414cad7b66fab
Yerkebulan-developer <58826624+Yerkebulan-developer@users.noreply.github.com>
2019-12-20 01:57:00 +0600

    Chapter 6: DotCom uluchshenie

--- DotComGame.java
+++ DotComGame.java
@@ -1,9 +1,9 @@
-public class SimpleDotComGame {
+public class DotComGame {
 	public static void main(String [] args) {
 		int numOfGuesses = 0;
 		GameHelper helper = new GameHelper();
 		
-		SimpleDotCom theDotCom = new SimpleDotCom ();
+		DotCom theDotCom = new DotCom ();
 		int randomNum = (int) (Math.random() * 5);
 		
 		int[] locations = {randomNum, randomNum+1, randomNum+2};
